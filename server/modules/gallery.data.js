f29e4ef3f375325a1689f0bcc48040401bcdabdd
Peter Lee <PeterLee2094@gmail.com>
2023-01-20 17:38:54 -0600

    created GET request to get gallery data

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,40 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: "images/bed.jpeg",
+    description: "Photo of a goat taken at Glacier National Park.",
+    likes: 0,
+  },
+  {
+    id: 2,
+    path: "images/dog1.jpeg",
+    description: "Photo of a goat taken at Glacier National Park.",
+    likes: 0,
+  },
+  {
+    id: 3,
+    path: "images/ice_cream.jpeg",
+    description: "Photo of a goat taken at Glacier National Park.",
+    likes: 0,
+  },
+  {
+    id: 4,
+    path: "images/lantern.jpeg",
+    description: "Photo of a goat taken at Glacier National Park.",
+    likes: 0,
+  },
+  {
+    id: 5,
+    path: "images/music.jpeg",
+    description: "Photo of a goat taken at Glacier National Park.",
+    likes: 0,
+  },
+  {
+    id: 6,
+    path: "images/theater.jpeg",
+    description: "Photo of a goat taken at Glacier National Park.",
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+
+  const [galleryList, setGalleryList] = useState([])
+
+  const fetchGallery = () => {
+    axios.get('/gallery')
+    .then((response)=>{
+      setGalleryList(response.data);
+    })
+    .catch((error)=>{
+      console.log('error with GET request, ', error);
+    })
+  }
+
+  useEffect(()=>{fetchGallery()}, []);
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {JSON.stringify(galleryList)}
       </div>
     );
 }
